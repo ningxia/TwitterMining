d3c5a920a603134020ad01dddad2b0a996921694
Ning Xia <brianalbertxia@gmail.com>
2012-12-16 04:20:34 -0500

    Fully functional

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.*;
 import play.db.jpa.JPA;
+import play.db.jpa.JPABase;
 import play.mvc.*;
 
 import java.util.*;
@@ -13,22 +14,6 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-    	render();
-    }
-    
-    public static void bounds(String zoomLevel, String swLat, String swLng, String neLat, String neLng) {
-//    	System.out.println("Southwest latitude: " + swLat);
-//    	System.out.println("Southwest longitude: " + swLng);
-//    	System.out.println("Northeast latitude: " + neLat);
-//    	System.out.println("Northeast longitude: " + neLng);
-    	int level = Integer.parseInt(zoomLevel);
-    	List<US> data = US.find("latitude >= ? and latitude <= ? and longitude >= ? and longitude <= ?", Float.parseFloat(swLat), Float.parseFloat(neLat), Float.parseFloat(swLng), Float.parseFloat(neLng)).fetch();
-    	System.out.println("Inside bounds: " + data.size());
-    	System.out.println("Zoom level: " + level);
-    	render(data);
-    }
-    
-    public static void sites() {
     	List<Site> siteList = Site.findAll();
     	int obama = 0;
     	int romney = 0;
@@ -36,9 +21,48 @@ public class Application extends Controller {
     		obama += siteList.get(i).obama;
     		romney += siteList.get(i).romney;
     	}
-    	String obamaStat = obama * 100 / (obama + romney) + "%";
-    	String romneyStat = romney * 100 / (obama + romney) + "%";
-    	Site site = new Site("", "US", obama, romney, null, null, obamaStat, romneyStat);
+    	// Map Center site
+    	float centerLat = 36.131294F;
+    	float centerLng = -95.937332F;
+    	Site site = new Site("", "US", centerLat, centerLng, obama, romney);
+    	render(site);
+    }
+    
+    public static int safeLongToInt(long l) {
+        if (l < Integer.MIN_VALUE || l > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException
+                (l + " cannot be cast to int without changing its value.");
+        }
+        return (int) l;
+    }
+    
+    public static void sites(String zoom, String swLat, String swLng, String neLat, String neLng) {
+    	
+    	int tweetsCountFilter = 10;
+    	int level = Integer.parseInt(zoom);
+    	
+    	List<Site> site = new ArrayList<Site>();
+    	Site s = null;
+    	Long obama = 0L;
+    	Long romney = 0L;
+    	// Nation wide view: show state capital statistics
+    	if(level < 5) {
+    		s = new Site("", "US", 36.131294F, -95.937332F, 6724, 5746);
+    		site.add(s);
+    	}
+    	else if(level < 6) {
+    		List<Capital> capitalList = Capital.findAll();
+    		for(Capital cap: capitalList) {
+    			obama = (Long) JPA.em().createQuery("SELECT SUM(s.obama) FROM Site s WHERE s.state = " + "'" + cap.state + "'").getResultList().get(0);
+    			romney = (Long) JPA.em().createQuery("SELECT SUM(s.romney) FROM Site s WHERE s.state = " + "'" + cap.state + "'").getResultList().get(0);
+    			s = new Site(cap.city, cap.state, cap.latitude, cap.longitude, safeLongToInt(obama), safeLongToInt(romney));
+    			site.add(s);
+    		}
+    	}
+    	else {
+    		site = Site.find("latitude >= ? and latitude <= ? and longitude >= ? and longitude <= ? and obama > ? and romney > ?", Float.parseFloat(swLat), Float.parseFloat(neLat), Float.parseFloat(swLng), Float.parseFloat(neLng), tweetsCountFilter, tweetsCountFilter).fetch();
+    	}
+    	
     	renderJSON(site);
     }
 
--- app/models/Site.java
+++ app/models/Site.java
@@ -25,13 +25,21 @@ public class Site extends Model {
 	
 	public String romneyStat;
 	
-	public Site(String city, String state, int obama, int romney, Float latitude, Float longitude, String obamaStat, String romneyStat) {
+	public Site(String city, String state, Float latitude, Float longitude, int obama, int romney) {
 		this.city = city;
 		this.state = state;
 		this.latitude = latitude;
 		this.longitude = longitude;
-		this.obamaStat = obamaStat;
-		this.romneyStat = romneyStat;
+		this.obama = obama;
+		this.romney = romney;
+		if(obama != 0 && romney != 0) {
+			this.obamaStat = obama * 100 / (obama + romney) + "%";
+			this.romneyStat = romney * 100 / (obama + romney) + "%";
+		}
+		else {
+			this.obamaStat = obama + "";
+			this.romneyStat = romney + "";
+		}
 	}
 	
 	public String toString() {
